097b13bc7b5d97251e8591131fc6777ac72a0236
viteacreator <83470337+viteacreator@users.noreply.github.com>
2024-06-13 19:12:36 +0300

    v1.1.2

---
+++ mesurement_bench_code/README.txt
@@ -0,0 +1,3 @@
+multimesurement_v1.1.2
+In this version was improvet the speed of writing to SD card the acumulated data, from 481ms for 500 variables, to 65ms for same data (500 variables).
+Also started to make my own libraris, an I started with ina219adc.
--- mesurement_bench_code/mesurement_bench_code.ino
+++ mesurement_bench_code/mesurement_bench_code.ino
@@ -3,20 +3,66 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <GyverINA.h>
+//#include <GyverINA.h>
+#include "INA219ADC.h"
+#include "string.h"
 
-const int chipSelectPin = 53;
+#define LOOPDELAY 1  //ms
+#define DISPUPDATEPERIOD 1600
+#define CHIPSELECTPIN 53
 
+#define DATABUFFERROWS 100  //for colectedData buffer max 255
+#define DATABUFFERCOLOMNS 4
+//#define PAKAGETOSDSIZE 10  // 10 rows of data from ram
+
+enum colData {
+  S1voltage,
+  S1current,
+  S2voltage,
+  S2current
+};
+//-----------------------------------------------------------------------------------------------------------
+uint16_t updateDispPeriod = 0;
+uint16_t prevUpdateDispTime = 0;
+
+uint16_t sensorReadPeriod = 0;
+uint16_t prevSensorReadTime = 0;
+
+uint16_t SDWritePeriod = 0;
+uint16_t prevSDWritePeriodTime = 0;
+
+uint16_t loopPeriod = 0;
+uint16_t prevLoopPeriodTime = 0;
+
+uint16_t updateLoopPeriod = 0;
+uint16_t prewUpdateLoopPeriod = 0;
+
+uint16_t dispWritePeriod = 0;
+uint16_t prewDispWritePeriod = 0;
+
+uint16_t dispUpdate = 0;  //for fixed display update
+uint32_t prewDispUpdate = 0;
+//-----------------------------------------------------------------------------------------------------------
+
+//uint8_t displayShowFlag = 0;
+bool SDLoggingState;
+
+//-----------------------------------------------------------------------------------------------------------
+
+int16_t colectedData[DATABUFFERROWS][DATABUFFERCOLOMNS];
+uint32_t colectedDataTiming[DATABUFFERROWS];
+//char colectedDataStringed[(DATABUFFERCOLOMNS + 1) * 5 + 1];
+
+int16_t displaiedData[DATABUFFERCOLOMNS];
+
+//-----------------------------------------------------------------------------------------------------------
 INA219 ina219_1(0.1f, 2.0f, 0x40);
 INA219 ina219_2(0.1f, 2.0f, 0x41);
+Adafruit_SSD1306 display(128, 64);
 
-// OLED display
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-
-void setup() {  //--------------------------------------------------------------------------------------------
+void setup() {  //-------------------------------------------------------------------------------------------
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //0x3C
 
   display.clearDisplay();
   display.setTextSize(1);
@@ -24,7 +70,7 @@ void setup() {  //--------------------------------------------------------------
   display.setCursor(0, 0);
   //---------------------------------------------------
   //---------------------------------------------------
-  while (!SD.begin(chipSelectPin)) {
+  while (!SD.begin(CHIPSELECTPIN)) {
     Serial.println("SD card initialization failed!");
     display.println("SD failed");
     display.display();
@@ -60,116 +106,201 @@ void setup() {  //--------------------------------------------------------------
     while (1)
       ;
   }
-  display.display();
 
   // Serial.print(F("Calibration value1: "));
   // Serial.println(ina219_1.getCalibration());
   // Serial.print(F("Calibration value2: "));
   // Serial.print(ina219_2.getCalibration());
-  ina219_1.setResolution(INA219_VBUS, INA219_RES_12BIT_X4);      // Напряжение в 12ти битном режиме + 4х кратное усреднение
-  ina219_1.setResolution(INA219_VSHUNT, INA219_RES_12BIT_X128);  // Ток в 12ти битном режиме + 128х кратное усреднение
-  ina219_2.setResolution(INA219_VBUS, INA219_RES_12BIT_X4);      // Напряжение в 12ти битном режиме + 4х кратное усреднение
-  ina219_2.setResolution(INA219_VSHUNT, INA219_RES_12BIT_X128);  // Ток в 12ти битном режиме + 128х кратное усреднение
+  ina219_1.setResolution(INA219_VBUS, INA219_RES_12BIT_X2);  // Напряжение в 12ти битном режиме + 4х кратное усреднение
+  ina219_1.setResolution(INA219_VSHUNT, INA219_RES_12BIT);   // Ток в 12ти битном режиме + 128х кратное усреднение
+  ina219_2.setResolution(INA219_VBUS, INA219_RES_12BIT_X2);  // Напряжение в 12ти битном режиме + 4х кратное усреднение
+  ina219_2.setResolution(INA219_VSHUNT, INA219_RES_12BIT);   // Ток в 12ти битном режиме + 128х кратное усреднение
 
   //---------------------------------------------------
+  //firstLogDataToSDCard();
   //---------------------------------------------------
-  File dataFile = SD.open("volamp.txt", FILE_WRITE);
-  if (dataFile) {
-    dataFile.println("S1Volt S1Amp S2Volt S2Amp time");
+  //reset all prew timers
 
-    dataFile.close();
-    Serial.println("Data saved to SD card.");
-    display.println("          SD saved");
-  } else {
-    Serial.println("Error opening data file on SD card.");
-    display.println("          SD error");
-  }
-
+  prewUpdateLoopPeriod = millis();
+  prevSensorReadTime = millis();
+  prevSDWritePeriodTime = millis();
+  prewDispWritePeriod = millis();
+  prevUpdateDispTime = millis();
   //---------------------------------------------------
 
-  // Serial.println("");
-
-  delay(2000);
+  display.display();
+  delay(1000);
 }
 
 void loop() {  //--------------------------------------------------------------------------------------------
   // Serial.println("Start");
-
   display.clearDisplay();
-  // display.setTextSize(0);
+  // display.setTextSize(1);
   // display.setTextColor(WHITE);
   display.setCursor(0, 0);
 
-  logDataToSDCard();
-  delay(100);
+  updateLoopPeriod = millis() - prewUpdateLoopPeriod;
+  prewUpdateLoopPeriod = millis();
 
-  display.display();
+  prevSensorReadTime = millis();
+  sensorsRead();
+  sensorReadPeriod = millis() - prevSensorReadTime;
+
+  prevSDWritePeriodTime = millis();
+  logDataToSDCard();
+  SDWritePeriod = millis() - prevSDWritePeriodTime;
+
+  if (millis() - prewDispUpdate >= DISPUPDATEPERIOD) {
+    prewDispUpdate = millis();  // for constant update
+
+    prewDispWritePeriod = millis();
+    printToDisplay();
+    dispWritePeriod = millis() - prewDispWritePeriod;  // for measuring display-print time
+
+    updateDispPeriod = millis() - prevUpdateDispTime;  // for measuring display-update time
+    prevUpdateDispTime = millis();
+  }
+
+  // delay(LOOPDELAY);
 
 }  //--------------------------------------------------------------------------------------------
 
-void logDataToSDCard() {
-  // Read values from sensor 1
-  float voltage1 = ina219_1.getVoltage();
-  float current1 = ina219_1.getCurrent();
+void computeDataForDisp() {
+  float S1volt = 0;
+  float S1curr = 0;
+  float S2volt = 0;
+  float S2curr = 0;
+  float pondere = 1 / (float)DATABUFFERROWS;
 
-  // Read values from sensor 2
-  float voltage2 = ina219_2.getVoltage();
-  float current2 = ina219_2.getCurrent();
+  for (uint8_t iteratie = 0; iteratie < DATABUFFERROWS; iteratie++) {
+    S1volt += pondere * (float)colectedData[iteratie][S1voltage];
+    S1curr += pondere * (float)colectedData[iteratie][S1current];
+    S2volt += pondere * (float)colectedData[iteratie][S2voltage];
+    S2curr += pondere * (float)colectedData[iteratie][S2current];
+  }
 
-  // Print to the serial monitor
-  Serial.print("S1:");
-  Serial.print(voltage1);
-  Serial.print("V,");
-  Serial.print(current1);
-  Serial.println("A");
-  Serial.print("S2:");
-  Serial.print(voltage2);
-  Serial.print("V,");
-  Serial.print(current2);
-  Serial.println("A");
+  displaiedData[S1voltage] = (uint16_t)S1volt;
+  displaiedData[S1current] = (int16_t)S1curr;
+  displaiedData[S2voltage] = (uint16_t)S2volt;
+  displaiedData[S2current] = (int16_t)S2curr;
+}
+
+void printToDisplay() {
+
+  computeDataForDisp();
 
   // Print to the display
   display.print("S1:");
-  display.print(voltage1, 2);
-  display.print("V ");
-  display.print(current1, 3);
-  display.print("A");
+  display.print(displaiedData[S1voltage]);
+  display.print("mV ");
+  // display.print((uint16_t)(voltage1*100));
+  // display.print("cV ");
+  display.print(displaiedData[S1current]);
+  display.print("mA");
   display.println();
   display.print("S2:");
-  display.print(voltage2, 2);
-  display.print("V ");
-  display.print(current2, 3);
-  display.println("A");
+  display.print(displaiedData[S2voltage]);
+  display.print("mV ");
+  display.print(displaiedData[S2current]);
+  display.println("mA");
 
+  if (SDLoggingState) {
+    // Serial.println("Data saved to SD card.");
+    display.println("          Logging...");
+  } else {
+    // Serial.println("Error opening data file on SD card.");
+    display.println("          SD error");
+  }
+
+  display.print("Du:");
+  display.print(updateDispPeriod);
+  display.print(" Lu:");
+  display.println(updateLoopPeriod);
+
+  display.print("Dw:");
+  display.print(dispWritePeriod);
+  display.print(" Sr:");
+  display.print(sensorReadPeriod);
+  display.print(" SDw:");
+  display.print(SDWritePeriod);
+  display.println();  //"ms");
+
+  display.display();
+}
+
+void sensorsRead() {
+  // Read values from sensors
+  for (uint8_t iteratie = 0; iteratie < DATABUFFERROWS; iteratie++) {
+
+    colectedData[iteratie][S1voltage] = (uint16_t)((ina219_1.getVoltage()) * 1000);
+    colectedData[iteratie][S1current] = (int16_t)((ina219_1.getCurrent()) * 1000);
+    colectedData[iteratie][S2voltage] = (uint16_t)((ina219_2.getVoltage()) * 1000);
+    colectedData[iteratie][S2current] = (int16_t)((ina219_2.getCurrent()) * 1000);
+    colectedDataTiming[iteratie] = millis();
+  }
+}
+
+void logDataToSDCard() {
   // Save the values to the SD card
   File dataFile = SD.open("volamp.csv", FILE_WRITE);
   if (dataFile) {
-    // dataFile.print("Sensor 1 - Voltage: ");
-    dataFile.print(voltage1, 3);
-    // dataFile.print("V, Current:");
-    dataFile.print(",");
-    dataFile.print(current1, 3);
-    // dataFile.println("A");
-    dataFile.print(",");
+    //------------------------------------------------------------------------------------
+    // String dataString = "";
 
-    // dataFile.print("Sensor 2 - Voltage: ");
-    dataFile.print(voltage2, 3);
-    // dataFile.print("V, Current:");
-    dataFile.print(",");
-    dataFile.print(current2, 3);
-    // dataFile.println("A");
-    dataFile.print(",");
+    // // Iterate through the data
+    // for (uint8_t iteratie = 0; iteratie < DATABUFFERROWS; iteratie++) {
+    //     // Append data for each row
+    //     dataString += String((uint16_t)(colectedData[iteratie][S1voltage])) + "," +
+    //                   String((int16_t)(colectedData[iteratie][S1current])) + "," +
+    //                   String((uint16_t)(colectedData[iteratie][S2voltage])) + "," +
+    //                   String((int16_t)(colectedData[iteratie][S2current])) + "," +
+    //                   String(colectedDataTiming[iteratie]) + "\n";
+    // }
 
-    dataFile.print(millis());
-    // dataFile.println("s");
-    dataFile.println(",");
+    // // Write the entire data string to the file
+    // dataFile.print(dataString);
+    //------------------------------------------------------------------------------------
+    // Prepare a character array to hold the entire matrix data
+    const size_t bufferSize = DATABUFFERROWS * (DATABUFFERCOLOMNS * (6 + 1) + 10 + 1) + 1;  // 6 digits per int16_t, 1 for comma, 10 digits per uint32_t, 1 for newline, 1 for null terminator
+    char dataBuffer[bufferSize];
+    size_t bufferIndex = 0;
 
+    //Iterate through the data
+    for (uint8_t iteratie = 0; iteratie < DATABUFFERROWS; iteratie++) {
+      // Append data for each row
+      bufferIndex += snprintf(dataBuffer + bufferIndex, bufferSize - bufferIndex - 1,  //-1 will prevent the null terminator on final data
+                              "%d,%d,%d,%d,%lu\n",
+                              (int16_t)(colectedData[iteratie][S1voltage]),
+                              (int16_t)(colectedData[iteratie][S1current]),
+                              (int16_t)(colectedData[iteratie][S2voltage]),
+                              (int16_t)(colectedData[iteratie][S2current]),
+                              (uint32_t)colectedDataTiming[iteratie]);
+    }
+    
+    // Write the entire data buffer to the file
+    dataFile.write((uint8_t*)dataBuffer, bufferIndex);
+    //------------------------------------------------------------------------------------
     dataFile.close();
-    Serial.println("Data saved to SD card.");
-    display.println("          Logging...");
+    // Serial.println("Data saved to SD card.");
+    // display.println("          Logging...");
+    SDLoggingState = 1;
   } else {
-    Serial.println("Error opening data file on SD card.");
+    // Serial.println("Error opening data file on SD card.");
+    // display.println("          SD error");
+    SDLoggingState = 0;
+    SD.begin(CHIPSELECTPIN);
+  }
+}
+
+void firstLogDataToSDCard() {
+  // Save the values to the SD card
+  File dataFile = SD.open("volamp.csv", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println("S1mVolt,S1mAmp,S2mVolt,S2mAmp,TimeIn_msFromStart");
+    dataFile.close();
+    display.println("          Logged...");
+  } else {
+    SD.begin(CHIPSELECTPIN);
     display.println("          SD error");
-    SD.begin(chipSelectPin);
   }
 }
